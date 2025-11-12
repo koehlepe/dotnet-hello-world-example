23d6da392de39ad00523d6ccdc3d232abce24731
Peter Butcher-Koehler <koehlepe@umich.edu>
2025-11-11 22:41:39 -0500

    First commit, just create a hello world

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_hello_world_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
