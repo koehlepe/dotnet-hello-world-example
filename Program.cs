0f22b3170d3297b815467efc679175f6f4c2e068
Peter Butcher-Koehler <koehlepe@umich.edu>
2025-11-12 13:28:17 -0500

    Updated dotnet version

--- Program.cs
+++ Program.cs
@@ -1,12 +1,2 @@
-﻿using System;
-
-namespace dotnet_hello_world_example
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
